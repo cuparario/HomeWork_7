1ee26d9c42da678ec9ba9ba229e513715a307057
Рамиль Шарапов <cuparario@yandex.ru>
2023-12-11 16:49:20 +0300

    Добавили домашнее задание 7

---
+++ Task_03/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 3: Задайте произвольный массив. Выведете его элементы, начиная 
+с конца. Использовать рекурсию, не использовать циклы. */
+
+        
+using System;
+
+namespace Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 1, 2, 3, 4, 5 };
+            ReverseArray(array);
+        }
+
+        private static void ReverseArray(int[] array)
+        {
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                var temp = array[i];
+                array[i] = array[array.Length - i - 1];
+                array[array.Length - i - 1] = temp;
+            }
+
+            Console.WriteLine("Результат: " + string.Join<int>(" ", array));
+        }
+    }
+}
